63e5b4e4021b1fdef9165e3f81b4440c7c653912
kaylahee <kaylahee0410@gmail.com>
2024-07-17 03:04:23 +0900

    controller test

--- Assets/SH_WorkSpace/Scripts/Test.cs
+++ Assets/SH_WorkSpace/Scripts/Test.cs
@@ -14,7 +14,6 @@ public class Test : MonoBehaviour
     public InputActionProperty leftMoveAction;
     public InputActionProperty rightMoveAction;
     public InputActionProperty leftRotateAction;
-    public InputActionProperty rightRotateAction;
     public InputActionProperty ascendAction;
     public InputActionProperty descendAction;
     public InputActionProperty switchCameraAction;
@@ -38,25 +37,29 @@ public class Test : MonoBehaviour
 
 	void HandleMovement()
 	{
+		// 왼쪽 조이스틱 입력 값 읽기
 		Vector2 leftMoveValue = leftMoveAction.action.ReadValue<Vector2>();
+		// 오른쪽 조이스틱 입력 값 읽기 (이동)
 		Vector2 rightMoveValue = rightMoveAction.action.ReadValue<Vector2>();
 
+		// 오른쪽 조이스틱을 통한 앞뒤 이동 및 좌우 이동
 		float moveForwardBackward = rightMoveValue.y * moveSpeed;
 		float moveLeftRight = rightMoveValue.x * moveSpeed;
-		float moveUpDown = 0f;
 
-		if (ascendAction.action.IsPressed())
-		{
-			moveUpDown = ascendSpeed;
-		}
-		else if (descendAction.action.IsPressed())
-		{
-			moveUpDown = -ascendSpeed;
-		}
+		// 왼쪽 조이스틱을 통한 상승/하강
+		float moveUpDown = leftMoveValue.y * ascendSpeed;
 
+		// 왼쪽 조이스틱을 통한 좌우 회전
+		float rotationAmount = leftMoveValue.x * rotationSpeed;
+
+		// 이동 벡터 계산
 		Vector3 movement = new Vector3(moveLeftRight, moveUpDown, moveForwardBackward);
 		Vector3 movementDirection = transform.TransformDirection(movement);
 
+		// 드론의 회전 설정
+		transform.Rotate(0, rotationAmount * Time.deltaTime, 0);
+
+		// 드론의 속도 설정
 		rb.velocity = movementDirection * Time.deltaTime;
 	}
 
