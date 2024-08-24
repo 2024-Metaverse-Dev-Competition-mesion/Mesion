e30b8782bd7aed1db77bce007556d9ab87468570
korysanan <ujin0561@naver.com>
2024-08-24 13:47:23 +0900

    ui 수정

--- Assets/WJ_WorkSpace/Question/Scripts/Practice_Mode_Select_Manager.cs
+++ Assets/WJ_WorkSpace/Question/Scripts/Practice_Mode_Select_Manager.cs
@@ -5,9 +5,30 @@ public class Practice_Mode_Select_Manager : MonoBehaviour
     // Reference to the other GameObject you want to activate
     public GameObject objectToActivate;
     public GameObject objectToDeactivate;
+    public GameObject Quiz_Random;
+    public GameObject Quiz_Part;
+    public GameObject Part_Text;
 
     public void OnButtonClick()
     {
+        Quiz_Part.SetActive(false);
+        // Deactivate the specified GameObject
+        if (objectToDeactivate != null)
+        {
+            objectToDeactivate.SetActive(false);
+        }
+
+        // Activate the other GameObject
+        if (objectToActivate != null)
+        {
+            objectToActivate.SetActive(true);
+        }
+    }
+
+    public void OnButtonClick2()
+    {
+        Quiz_Random.SetActive(false);
+        Part_Text.SetActive(false);
         // Deactivate the specified GameObject
         if (objectToDeactivate != null)
         {
