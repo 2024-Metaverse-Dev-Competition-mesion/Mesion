fa4625dc077ea06bcc3965a47b1f0a958846fb03
kaylahee <kaylahee0410@gmail.com>
2024-09-10 08:45:46 +0900

    add afterflight scene

---
+++ Assets/WorkSpace/SH_WorkSpace/Scripts/Script for drone after flight/Step01.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fee76b37f4a37fad6d6840dacd8f9f344ae07652cebb88dba00584e68e23e7c4
+size 3501
--- Assets/WorkSpace/SH_WorkSpace/Scripts/Script for new drone/CheckPointController.cs
+++ Assets/WorkSpace/SH_WorkSpace/Scripts/Script for new drone/CheckPointController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c3b0af23aeb5451ac26573011937ec74e526eaa8f4e41583e4fb3d089e7aba7
-size 1045
+oid sha256:7000d48d7b339888a97ee204a02b0948b9e653a528cf2e0d757b9b72eb7fba06
+size 1044
